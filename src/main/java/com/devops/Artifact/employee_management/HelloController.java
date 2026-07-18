47390dd17fa12550c86c202f4f32f6a5b7a5b688
kalilmemes-eng <kalilmemes@gmail.com>
2026-07-18 20:41:01 +0530

    Add HelloController endpoint

---
+++ src/main/java/com/devops/Artifact/employee_management/HelloController.java
@@ -0,0 +1,13 @@
+package com.devops.Artifact.employee_management;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Jenkins Docker Pipeline!";
+    }
+}
