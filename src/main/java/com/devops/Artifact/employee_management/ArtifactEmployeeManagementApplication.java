6fd9a66cfb908e832f3687ffbc9d2e72ed64439f
kalilmemes-eng <kalilmemes@gmail.com>
2026-07-18 17:52:21 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/devops/Artifact/employee_management/ArtifactEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.devops.Artifact.employee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/Artifact/employee_management/ArtifactEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.Artifact.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
