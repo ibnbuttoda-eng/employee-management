43ca2e8234958b4cdd6346572b97c319bfab9af2
kalilmemes-eng <kalilmemes@gmail.com>
2026-07-19 14:53:50 +0530

    Add Employee REST API

---
+++ src/main/java/com/devops/Artifact/employee_management/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.devops.Artifact.employee_management;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public EmployeeController() {
+        employees.add(new Employee(1, "John", "IT"));
+        employees.add(new Employee(2, "Alice", "HR"));
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employees;
+    }
+}
